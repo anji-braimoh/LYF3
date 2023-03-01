1d2ea266ade2f5614e81d7c090bd417f4b90f735
anji-braimoh <anjolabraimoh@outlook.com>
2023-03-01 13:22:23 +0000

    completed the infinite pomodoro timer

--- lyf3/src/components/study.js
+++ lyf3/src/components/study.js
@@ -1,3 +1,49 @@
+import React, {useState,useEffect} from "react"
+import './study.css';
+
+
+
 export function Study() {
-    return <h1>Study Room</h1>
+const [minutes,setMinutes] = useState(25)
+const [seconds,setSeconds]= useState(0)
+const [displayMessage,setDisplayMessage]= useState(false)
+
+useEffect(() => {
+  let interval = setInterval(()=>{
+clearInterval(interval)
+
+if (seconds===0){
+    if (minutes!==0){
+        setSeconds(59)
+        setMinutes(minutes - 1)
+    } else {
+        let minutes = displayMessage ? 24:14
+        let seconds = 59
+
+        setSeconds(seconds)
+        setMinutes(minutes)
+        setDisplayMessage(!displayMessage)
+    }
+} else{
+    setSeconds (seconds -1)
+}
+  },1000)
+}, [seconds]) //gives us a side effect we can run whenever something happens, so each time we update seconds, we want something to happen in use effect
+
+
+const timerMinutes = minutes < 10 ? `0${minutes}` : minutes
+const timerSeconds = seconds < 10 ? `0${seconds}`: seconds
+ 
+
+    return <div className="pomodoro">
+        <p>Infinite Pomodoro Timer</p>
+        <p className="info">"25 minute study session with a 15 minute Break"</p>
+        <div className="message">
+            {displayMessage && <div>Break Time! New Session starts in :</div>}
+        </div>
+        <div className="timer">
+            {timerMinutes}:{timerSeconds}
+            </div>
+</div>
+    
 }
